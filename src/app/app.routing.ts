8278b0b900759d50fdb08069c85bdcb0ece62b11
Jhonatan <jdatuesta7@gmail.com>
2021-10-16 15:47:18 -0500

    vista de productos lista

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from '@angular/core';
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { ContactoComponent } from "./components/contacto/contacto.component";
+import { ProductosComponent } from "./components/productos/productos.component";
 
 const appRoute: Routes = [
     {path: '', component: InicioComponent},
-    {path: 'contacto', component: ContactoComponent}
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'productos', component: ProductosComponent}
 ];
 
 export const appRoutingProviders : any[] = [];
