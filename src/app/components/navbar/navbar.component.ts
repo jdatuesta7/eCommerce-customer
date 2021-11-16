c029e01466fbf7098c6b9e36bfb07c7477602dee
Jhonatan <jdatuesta7@gmail.com>
2021-11-15 22:45:20 -0500

    cerrar sesion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
@@ -14,7 +15,8 @@ export class NavbarComponent implements OnInit {
   public usuario_datos : any = {};
 
   constructor(
-    private _clienteService: ClienteService
+    private _clienteService: ClienteService,
+    private _router: Router
   ) { 
     this.token = localStorage.getItem('token');
     this.id = localStorage.getItem('_id');
@@ -44,6 +46,12 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    window.location.reload();
+    localStorage.clear();
+    this._router.navigate(['/']);
   }
 
 }
