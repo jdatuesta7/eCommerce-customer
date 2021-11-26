886a9f4fe77a641b6d4ba6c5c5eb6eb089879478
Jhonatan <jdatuesta7@gmail.com>
2021-11-25 20:16:10 -0500

    eliminada cantidad excesiva de producto

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,4 +1,7 @@
 export var GLOBAL = {
     url: 'http://127.0.0.1:4201/api/',
-    url2: 'https://central-park-ecommerce.herokuapp.com/api/'
+    urlSocketLocal: 'http://localhost:4201',
+    
+    urlPublica: 'https://central-park-ecommerce.herokuapp.com/api/',
+    urlSocketPublico: 'https://central-park-ecommerce.herokuapp.com' 
 }
